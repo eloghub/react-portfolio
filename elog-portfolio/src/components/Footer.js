cb1a6e932735a1739199dc8432a61df32001665e
eloggit <elogtechmail@gmail.com>
2021-06-18 14:09:16 -0400

    updated Footer.js footer

--- elog-portfolio/src/components/Footer.js
+++ elog-portfolio/src/components/Footer.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "../styles/Footer.css";
 
-// By importing the Header.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
-
 const styles = {
   footerStyle: {
     background: "green"
@@ -22,7 +15,7 @@ const styles = {
 function Footer() {
   return (
     <header style={styles.footerStyle} className="footer">
-      <h1 style={styles.footingStyle}>2021</h1>
+      <h1 style={styles.footingStyle}>© ELOGHUB 2021</h1>
     </header>
   );
 }
